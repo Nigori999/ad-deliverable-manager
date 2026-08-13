165e142229332a75e66987be6173b0276b279c12
清茶浊酒 <43953340+Nigori999@users.noreply.github.com>
2026-08-13 16:55:35 +0800

    fix: restore PasswordService namespace in auth controller

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 using System.Security.Claims;
 using AdDeliverableManager.Models;
+using AdDeliverableManager.Security;
 using AdDeliverableManager.Services;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
